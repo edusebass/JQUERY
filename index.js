749b05d16ab4c74a2007f4efd48b204cc2ededac
edusebass <eduardoalmachi123@hotmail.com>
2023-04-11 21:53:30 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-$("h1").css("color", "red");
+$("h1").css("color", "red"); // properties css with first parameter is the method and the last parameter is the a propertie the paramete
+$("button"); // $ = document.querySelector
+
+console.log($("button", "green")); //this you can see the properties
+
