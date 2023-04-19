bcfde829a0bdf35be5e68e0dfaf79dac594c7cd6
edusebass <eduardoalmachi123@hotmail.com>
2023-04-18 22:49:46 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-$("h1").css("color", "red"); // properties css with first parameter is the method and the last parameter is the a propertie the paramete
-$("button"); // $ = document.querySelector
+// $("h1").css("color", "red"); // properties css with first parameter is the method and the last parameter is the a propertie the paramete
+// $("button"); // $ = document.querySelector
 
-console.log($("button", "green")); //this you can see the properties
+// console.log($("button", "green")); //this you can see the properties
 
 $("h1"), addClass("bit-title"); // big-title was created in the styles.css
 
@@ -9,3 +9,8 @@ $("button").text("<em>Hey</em>"); // chANGE THE TEXT OF THE BUTTON
 
 //Manipuling attributes with Jquery
 $("a").attr("href", "https://www.github.com")
+
+//Adding event listeners with Jquery
+$(button).click(function() {
+    $(h1).css("color", "purple");
+});
