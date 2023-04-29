4721872f7127acd4fbd4c0da085b7dbf1030b478
edusebass <eduardoalmachi123@hotmail.com>
2023-04-28 19:24:18 -0500

    update

--- teory-practice/index.js
+++ teory-practice/index.js
@@ -13,4 +13,11 @@ $("a").attr("href", "https://www.github.com")
 //Adding event listeners with Jquery
 $("button").click(function() {
     $("h1").css("color", "purple");
+    $(this).addClass("active"); // agrega la clase "active" al botón que se hizo clic
+    $("button").not(this).removeClass("active"); // quita la clase "active" de todos los demás botones
+});
+
+$("button.active").click(function() {
+    $("h1").css("color", ""); // restablece el color original del texto
+    $("button").removeClass("active"); // quita la clase "active" de todos los botones
 });
